860b7a0f782a26ac638c8731e18e45c10c6b74da
Sahitya <sp3133@nyu.edu>
2018-04-22 15:25:38 -0400

    Adding problem gigasecond

---
+++ go/gigasecond/gigasecond.go
@@ -0,0 +1,11 @@
+// Package gigasecond has a method to return the time after adding giga seconds
+package gigasecond
+
+import "time"
+
+// AddGigasecond adds and returns the time after giga seconds
+func AddGigasecond(t time.Time) time.Time {
+	// tt store the time after giga seconds from t
+	tt := t.Add(time.Second * 1e9)
+	return tt
+}
