3a2f8b3a6dd99b03150bec9c2f028a0b66fde5fd
sahitya-pavurala <sp3133@nyu.edu>
2018-04-23 01:13:34 -0400

    problem3: adding acronym

---
+++ go/acronym/acronym.go
@@ -0,0 +1,31 @@
+// Package acronym has method to give acronyms for strings
+package acronym
+
+import "strings"
+
+// Abbreviate returns an acronym for s
+func Abbreviate(s string) string {
+	abb := ""
+	flag := true
+	for i, r := range s {
+		if isAlpha(r) {
+			if flag {
+				abb += strings.ToUpper(s[i : i+1])
+				flag = false
+			}
+		} else {
+			flag = true
+		}
+
+	}
+	return abb
+}
+
+// isAlpha checks if character is alphabet
+func isAlpha(s rune) bool {
+	if !(s <= 'z' && s >= 'a') && !(s <= 'Z' && s >= 'A') {
+		return false
+	}
+
+	return true
+}
