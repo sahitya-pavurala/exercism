5c2887cb0ef0cb746668ee28714d0b350157a36f
sahitya-pavurala <sp3133@nyu.edu>
2018-04-26 05:32:19 -0400

    problem8: adding scrabble-score

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,32 @@
+// Package scrabble has methods to give score of a word
+package scrabble
+
+import "strings"
+
+// Score give the scrabble score of a word
+func Score(word string) int {
+
+	result := 0
+	word = strings.ToUpper(word)
+	for _, c := range word {
+		switch c {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			result += 1
+		case 'D', 'G':
+			result += 2
+		case 'B', 'C', 'M', 'P':
+			result += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			result += 4
+		case 'K':
+			result += 5
+		case 'J', 'X':
+			result += 8
+		case 'Q', 'Z':
+			result += 10
+		}
+
+	}
+
+	return result
+}
