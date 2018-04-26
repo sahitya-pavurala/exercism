a585ee1fa1c96644047b6ec06c7788cde2d88aee
sahitya-pavurala <sp3133@nyu.edu>
2018-04-26 05:51:44 -0400

    problem9: adding isogram

---
+++ go/isogram/isogram.go
@@ -0,0 +1,21 @@
+// Package isogram provides methods to check is string is isogram
+package isogram
+
+import "strings"
+
+// IsIsogram returns true if word is a isogram, false otherwise
+func IsIsogram(word string) bool {
+	checker := make(map[rune]bool)
+
+	word = strings.ToLower(word)
+
+	for _, c := range word {
+		if c >= 'a' && c <= 'z' {
+			if _, ok := checker[c]; ok {
+				return false
+			}
+			checker[c] = true
+		}
+	}
+	return true
+}
